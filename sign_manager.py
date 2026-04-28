22b0a2e8963fee126b95a061310d03079ba14d4e
kauanysantos20 <kauanysantos.ls04@gmail.com>
2026-04-28 14:34:44 -0300

    Adicionando o sign_manager

--- sign_manager.py
+++ sign_manager.py
@@ -1,1 +1,33 @@
-# Assinatura digital (RSA-PSS) e validação de autenticidade
+from cryptography.hazmat.primitives.asymmetric import padding as rsa_padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
+
+def gerar_assinatura(mensagem, chave_privada_remetente):
+    payload = mensagem.encode("utf-8")
+    
+    assinatura = chave_privada_remetente.sign(
+        payload,
+        rsa_padding.PSS(
+            mgf=rsa_padding.MGF1(hashes.SHA256()),
+            salt_length=rsa_padding.PSS.MAX_LENGTH,
+        ),
+        hashes.SHA256(),
+    )
+    return assinatura
+
+def verificar_assinatura(mensagem, assinatura, chave_publica_remetente):
+    payload = mensagem.encode("utf-8")
+    
+    try:
+        chave_publica_remetente.verify(
+            assinatura,
+            payload,
+            rsa_padding.PSS(
+                mgf=rsa_padding.MGF1(hashes.SHA256()),
+                salt_length=rsa_padding.PSS.MAX_LENGTH,
+            ),
+            hashes.SHA256(),
+        )
+        return True
+    except InvalidSignature:
+        return False
